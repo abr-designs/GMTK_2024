091c19c4118b1e8143bfba97006c9a37c1fd5ef0
Alex Bedard-Reid <alexbedardreid@gmail.com>
2024-08-17 00:36:54 -0700

    Implemented Systems
    [ADD] Added Game Scene
    [ADD] Added Primitives & Color Materials
    [ADD] Added PrintLayerShapeScriptableObject to contain data about a shape
    [MOD] Changed LevelController to LevelData
    [MOD] Added basic data into LevelData to ensure smooth game flow

---
+++ Assets/Scripts/Levels/LayerData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Levels
+{
+    [Serializable]
+    public class LayerData
+    {
+        public Material Material;
+        public PrintLayerShapeScriptableObject printLayerShape;
+        //This should contain the scene ref to what was intended
+        //public Transform sceneRefTransform;
+        public Vector2 localPosition;
+        public float yRotation;
+        public Vector2 localScale = Vector2.one;
+    }
+}
