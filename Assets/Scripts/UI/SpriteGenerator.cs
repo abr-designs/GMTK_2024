da52ccb1fc31cef1103ccad6bda3f1323c5be562
tyzor <74879636+tyzor@users.noreply.github.com>
2024-08-17 21:02:26 -0400

    Sprite System complete

--- Assets/Scripts/UI/SpriteGenerator.cs
+++ Assets/Scripts/UI/SpriteGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Interfaces;
 using Levels;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public enum SPRITE_VIEW {
@@ -23,13 +24,27 @@ public class SpriteGenerator : MonoBehaviour, IGenerateSilhouette
     [SerializeField]
     private Transform cameraTargetContainer;
 
+    private int _activeLayerIndex = 0;
+    private SPRITE_VIEW _currentView = SPRITE_VIEW.SIDE_VIEW;
+    private LevelDataContainer _currentLevel;
+
     public void BuildSilhouette(LevelDataContainer levelDataContainer)
     {
-        BuildLevel(levelDataContainer);
+        _currentLevel = levelDataContainer;
+        BuildLevel();
+    }
+
+    public void ToggleView() {
+        // why doesn't this work!!!!
+        //_currentView = ((int)_currentView + 1) % (System.Enum.GetValues(typeof(SPRITE_VIEW)).Length);
+        _currentView = (_currentView == SPRITE_VIEW.SIDE_VIEW) ? SPRITE_VIEW.ISO_VIEW : SPRITE_VIEW.SIDE_VIEW;
+        UpdateView();
     }
 
     // Instantiate each layer
-    private void BuildLevel(LevelDataContainer levelDataContainer) {
+    private void BuildLevel() {
+
+        LevelDataContainer levelDataContainer= _currentLevel;
 
         // Clear existing prefabs from camera
         foreach(Transform t in cameraTargetContainer.transform) {
@@ -52,27 +67,51 @@ public class SpriteGenerator : MonoBehaviour, IGenerateSilhouette
             //layerObject.GetComponent<MeshRenderer>().material.color = color;
         }
 
-        // TODO -- remove
-        CaptureView(levelDataContainer, SPRITE_VIEW.ISO_VIEW);
+        UpdateView();
 
     }
 
-    public void CaptureView(LevelDataContainer levelDataContainer, SPRITE_VIEW viewType) {
+    public void UpdateView() {
         
+        if(!_currentLevel) return;
+
+        // Alter materials based on current layer
+        for(int i=0; i<_currentLevel.layers.Length; i++)
+        {
+            var child = cameraTargetContainer.GetChild(i);
+            var renderer = child.GetComponent<MeshRenderer>();
+            if(_activeLayerIndex == i)
+            {
+                renderer.enabled = true;
+                // Set to green
+                // TODO -- use custom material?
+                renderer.material.color = Color.green;
+            } 
+            /*else if(_activeLayerIndex < i)
+            {
+                renderer.enabled = false;   
+            }*/ else {
+                renderer.enabled = true;
+                renderer.material.color = Color.gray;
+            }
+
+        }
+
+
         spriteCamera.enabled = true;
 
-        if(viewType == SPRITE_VIEW.SIDE_VIEW)
+        if(_currentView == SPRITE_VIEW.SIDE_VIEW)
         {
-            spriteCamera.transform.localPosition = new Vector3(0,((levelDataContainer.layers.Length / 2f) - 0.5f),-5f);
+            spriteCamera.transform.localPosition = new Vector3(0,((_currentLevel.layers.Length / 2f) - 0.5f),-5f);
             spriteCamera.transform.localRotation = Quaternion.identity;
-            spriteCamera.orthographicSize = levelDataContainer.layers.Length * levelDataContainer.yScale;
-        } else if (viewType == SPRITE_VIEW.ISO_VIEW)
+            spriteCamera.orthographicSize = _currentLevel.layers.Length * _currentLevel.yScale;
+        } else if (_currentView == SPRITE_VIEW.ISO_VIEW)
         {
 
             spriteCamera.transform.localPosition = Vector3.zero;
-            spriteCamera.transform.localPosition = new Vector3(-5,5 + ((levelDataContainer.layers.Length / 2f) - 0.5f),-5);
+            spriteCamera.transform.localPosition = new Vector3(-5,5 + ((_currentLevel.layers.Length / 2f) - 0.5f),-5);
             spriteCamera.transform.localRotation = Quaternion.Euler(new Vector3(35.264f,45,0));
-            spriteCamera.orthographicSize = (levelDataContainer.layers.Length + 1) * levelDataContainer.yScale;
+            spriteCamera.orthographicSize = (_currentLevel.layers.Length + 1) * _currentLevel.yScale;
         }
 
         // Take picture (renderTexture)
@@ -82,17 +121,36 @@ public class SpriteGenerator : MonoBehaviour, IGenerateSilhouette
         spriteCamera.enabled = false;
 
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
+  
+    void OnEnable() {
+        GameManager.OnLayerSelected += OnLayerSelected;
+    }
+    void OnDisable() {
+        GameManager.OnLayerSelected -= OnLayerSelected;
     }
 
-  
+    void OnLayerSelected(int index) {
+        _activeLayerIndex = index;
+        UpdateView();   
+    }
 
     [ContextMenu("Sprite Preview")]
     void SpritePreview()
     {
         BuildSilhouette(_debugLevel);
     }
+
+    [ContextMenu("Debug - Increase Layer")]
+    void IncreaseLayer()
+    {
+        OnLayerSelected(_activeLayerIndex+1);
+    }
+
+    [ContextMenu("Debug - Toggle View")]
+    void ToggleViewDebug()
+    {
+        ToggleView();
+    }
+    
+
 }
