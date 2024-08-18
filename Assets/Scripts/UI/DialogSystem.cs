91c977d76a14f9b81c3428dc7bcdae0b2d1ab5a5
CieroCeramics <red_wolf19@hotmail.com>
2024-08-17 22:53:19 -0700

    adding tmp asset and script changes
    adding functionality to DialogueSystem script.
    adding test text to display typewriter effect on a canvas.

--- Assets/Scripts/UI/DialogSystem.cs
+++ Assets/Scripts/UI/DialogSystem.cs
@@ -1,20 +1,37 @@
+using System;
 using System.Collections;
 using Interfaces;
+using TMPro;
 using UnityEngine;
 
 namespace UI
 {
     public class DialogSystem : MonoBehaviour, IDisplayDialog
     {
+
+        public TMP_Text container;
         // Start is called before the first frame update
         void Start()
         {
-        
+            //StartCoroutine(DisplayDialogCoroutine("this is a test"));
         }
 
+        int tick = 0;
         public IEnumerator DisplayDialogCoroutine(string text)
         {
+
+            container.text = text;
+            int text_length = text.Length;
+            for (int i = 0; i < text_length + 2; i++)
+            {
+                container.maxVisibleCharacters = i;
+                yield return new WaitForSeconds(0.1f);
+            }
+            //tick++;
             yield break;
+
         }
+
+
     }
 }
